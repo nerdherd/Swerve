1a3db56043a0b6bd0c3d8517b8abceec2cad4425
Tiffany Chen <tchen1914@gmail.com>
2022-03-28 13:24:38 -0700

    Module setup

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,36 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+    public static final double kWheelRadius = 0;
+    public static final int kEncoderResolution = 4096;
+
+    public static final double kModuleMaxAngularVelocity = 0;
+    public static final double kModuleMaxAngularAcceleration = 2 * Math.PI; // radians per seconds squared
+    public static final double kMaxAngularSpeed = Math.PI;
+
+    public static final double kDriveControllerkP = 0;
+    public static final double kDriveControllerkI = 0;
+    public static final double kDriveControllerkD = 0;
+
+    public static final double kTurningControllerkP = 0;
+    public static final double kTurningControllerkI = 0;
+    public static final double kTurningControllerkD = 0;
+
+    public static final double kDriveFeedForwardkS = 0;
+    public static final double kDriveFeedForwardkV = 0;
+    public static final double kTurningFeedForwardkS = 0;
+    public static final double kTurningFeedForwardkV = 0;
+
+}
